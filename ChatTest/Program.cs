c432fbb9cf52e3e6ff777d6a07fcee2c3f121103
zx412563 <98372031+zx412563@users.noreply.github.com>
2022-07-23 13:17:22 +0800

    first commit

---
+++ ChatTest/Program.cs
@@ -0,0 +1,40 @@
+using ChatTest.Hubs;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// 加入 signalR
+
+builder.Services.AddSignalR();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//加入Hubs
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
